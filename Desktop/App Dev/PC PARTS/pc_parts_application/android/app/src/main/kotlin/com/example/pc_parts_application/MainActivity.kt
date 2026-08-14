a772c10f2157c0077e66415c53658bce4c639293
Lascano <lascanoj41@gmail.com>
2026-08-14 20:37:54 +0800

    Fix project directory structure

--- Desktop/App Dev/PC PARTS/pc_parts_application/android/app/src/main/kotlin/com/example/pc_parts_application/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pc_parts_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Desktop/App Dev/PC PARTS/pc_parts_application/lib/core/enums/user_role.dart
+++
@@ -1,5 +0,0 @@
-enum UserRole {
-  customer,
-  staff,
-  admin,
-}
